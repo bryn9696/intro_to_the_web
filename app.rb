47b26d9eb26dde9bf5e2c0a3268088ff5e52be66
Bryn <bryn2009@live.co.uk>
2021-11-29 15:49:43 +0000

    Returning HTML

---
+++ app.rb
@@ -0,0 +1,17 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+
+
+get '/' do
+  "hello!"
+end
+
+get '/secret' do
+  "hi there!!"
+end
+
+get '/cat' do
+  "<div>
+  <img src = 'https://i.imgur.com/jFaSxym.png'>
+  </div>"
+end
