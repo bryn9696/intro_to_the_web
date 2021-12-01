55db863534af1d43021db15cb3d2305cbbe54ba6
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2021-12-01 15:21:16 +0000

    Step#22 Refactor

--- battle/spec/features/enter_names_spec.rb
+++ battle/spec/features/enter_names_spec.rb
@@ -5,11 +5,3 @@ feature 'Enter names' do
       expect(page).to have_content 'Michael vs. Bryn'
     end
 end
-
-feature 'Hit points' do
-  scenario 'I want to see Player 2s hit points' do
-    sign_in_and_play
-    save_and_open_page
-    expect(page).to have_content 'Bryn 100HP'
-  end
-end
---
+++ battle/spec/features/hit_points_spec.rb
@@ -0,0 +1,7 @@
+feature 'Hit points' do
+  scenario 'I want to see Player 2s hit points' do
+    sign_in_and_play
+    save_and_open_page
+    expect(page).to have_content 'Bryn 100HP'
+  end
+end
