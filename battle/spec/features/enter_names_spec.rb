a47f40666b2b443e9a896239373e015550c8eff6
Bryn <bryn2009@live.co.uk>
2021-12-01 15:08:44 +0000

    #21 viewing hit points

--- battle/spec/features/enter_names_spec.rb
+++ battle/spec/features/enter_names_spec.rb
@@ -7,4 +7,15 @@ feature 'Enter names' do
       save_and_open_page
       expect(page).to have_content 'Michael vs. Bryn'
     end
+end
+
+feature 'Hit points' do
+  scenario 'I want to see Player 2s hit points' do
+    visit('/')
+    fill_in :player_1_name, with: 'Michael'
+    fill_in :player_2_name, with: 'Bryn'
+    click_button 'Submit'
+    save_and_open_page
+    expect(page).to have_content 'Bryn 100HP'
   end
+end
