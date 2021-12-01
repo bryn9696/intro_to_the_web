25ed9b4e97a559e088b1ae9fbac933db52726dd9
Bryn <bryn2009@live.co.uk>
2021-12-01 17:41:34 +0000

    #25 not finished

--- battle/spec/features/attack_spec.rb
+++ battle/spec/features/attack_spec.rb
@@ -3,6 +3,13 @@ feature 'Attack' do
     sign_in_and_play
     save_and_open_page
     click_link "Attack"
-    expect(page).to have_content 'Michael attacks'
+    expect(page).to have_content "Michael attacks"
+  end
+#I want my attack to reduce Player 2's HP by 10
+  scenario 'when player 1 attacks, reduce Player 2s HP by 10' do
+    sign_in_and_play
+    save_and_open_page
+    click_link "Attack"
+    expect(page).to have_content "Michael attacks, Bryn 50HP"
   end
 end
--- battle/spec/player_spec.rb
+++ battle/spec/player_spec.rb
@@ -1,7 +1,29 @@
 describe Player do
+  subject(:michael) { Player.new('Michael') }
+  subject(:bryn) { Player.new('Bryn') }
 
-	it 'names the Player' do
-		michael = Player.new("Michael")
-		expect(michael.name).to eq "Michael"
-	end
-end
+  describe '#name' do
+    it 'returns the name' do
+      expect(michael.name).to eq 'Michael'
+    end
+  end
+
+  describe '#hit_points' do
+    it 'returns the hit points' do
+      expect(michael.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(bryn).to receive(:receive_damage)
+      michael.attack(bryn)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'reduces the player hit points' do
+      expect { michael.receive_damage }.to change { michael.hit_points }.by(-10)
+    end
+  end
+ end
